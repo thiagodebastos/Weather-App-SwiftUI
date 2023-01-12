19117be2e1920f81959969d3814ab22b83d97773
Thiago de Bastos <6151341+thiagodebastos@users.noreply.github.com>
2023-01-12 19:18:27 +1100

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Thiago de Bastos on 12/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
